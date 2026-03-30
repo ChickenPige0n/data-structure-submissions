220e8cd76ca3fe237ea8b87af1491e51f671b22e
ChickenPige0n <2336983354@qq.com>
2026-03-30 14:16:34 +0800

    chore: init template

---
+++ xmake.lua
@@ -0,0 +1,11 @@
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate")
+set_runtimes("MT")
+set_toolchains("clang")
+
+set_languages("c23")
+
+target("project")
+    add_cflags("-Wno-deprecated-declarations")
+    set_kind("binary")
+    add_files("solution.c")
