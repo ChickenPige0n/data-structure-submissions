aaa3d218641b6929843147d22c1f97300d10c82d
ChickenPige0n <2336983354@qq.com>
2026-03-30 14:16:34 +0800

    init

---
+++ solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Solution();
+
+int main(int argc, char** argv) {
+    Solution();
+    return 0;
+}
+
+void Solution() {
+    printf("Hello World");
+}
