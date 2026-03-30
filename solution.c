d678bddc3dfcd9a0fbf055da661a2d07c2c27715
ChickenPige0n <2336983354@qq.com>
2026-03-30 14:27:49 +0800

    feat: implement

--- solution.c
+++ solution.c
@@ -8,6 +8,29 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void visit_preorder(int elem_count, int index) {
+    if (index > elem_count) return;
+    printf("%d ", index);
+    visit_preorder(elem_count, index * 2);
+    visit_preorder(elem_count, index * 2 + 1);
+}
+
+void visit_inorder(int elem_count, int index) {
+    if (index > elem_count) return;
+    visit_inorder(elem_count, index * 2);
+    printf("%d ", index);
+    visit_inorder(elem_count, index * 2 + 1);
+}
+
+void visit_postorder(int elem_count, int index) {
+    if (index > elem_count) return;
+    visit_postorder(elem_count, index * 2);
+    printf("%d ", index);
+    visit_postorder(elem_count, index * 2 + 1);
+}
+
 void Solution() {
-    printf("Hello World");
+    int layer_count;
+    scanf("%d", &layer_count);
+    visit_inorder(layer_count, 1);
 }
