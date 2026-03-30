f21fd4da9a61bd95e503af21f6aa002b3fdb47cc
ChickenPige0n <2336983354@qq.com>
2026-03-30 14:28:16 +0800

    chore: remove dead code

--- solution.c
+++ solution.c
@@ -8,25 +8,11 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void visit_preorder(int elem_count, int index) {
-    if (index > elem_count) return;
-    printf("%d ", index);
-    visit_preorder(elem_count, index * 2);
-    visit_preorder(elem_count, index * 2 + 1);
-}
-
 void visit_inorder(int elem_count, int index) {
     if (index > elem_count) return;
     visit_inorder(elem_count, index * 2);
     printf("%d ", index);
     visit_inorder(elem_count, index * 2 + 1);
-}
-
-void visit_postorder(int elem_count, int index) {
-    if (index > elem_count) return;
-    visit_postorder(elem_count, index * 2);
-    printf("%d ", index);
-    visit_postorder(elem_count, index * 2 + 1);
 }
 
 void Solution() {
