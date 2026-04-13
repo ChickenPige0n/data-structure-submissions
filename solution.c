6bf63a48589d6c729cc9a95bcae69f8ff849bffd
ChickenPige0n <2336983354@qq.com>
2026-04-13 13:54:44 +0800

    feat: lab_0413_1 implementation

--- solution.c
+++ solution.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +9,24 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+/// index starts from 1. tree[0] == tree.count
+void mid_order(int* tree, int index) {
+    if (index > tree[0]) return;
+    mid_order(tree, index * 2);
+    printf("%d ", tree[index]);
+    mid_order(tree, index * 2 + 1);
+}
+
 void Solution() {
-    printf("Hello World");
+    int n;
+    scanf("%d", &n);
+    int* tree = malloc(sizeof(int) * (n + 1));
+    tree[0] = n;
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &tree[i]);
+    }
+    mid_order(tree, 1);
+
+    int depth = ceil(log2(n));
+    printf("\n%d", depth);
 }
