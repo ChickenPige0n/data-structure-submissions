a9d2b70d50e13d2e4ab05a6ade22fbb76baaace6
ChickenPige0n <2336983354@qq.com>
2026-04-13 14:17:51 +0800

    feat: lab_0413_2 implementation

--- solution.c
+++ solution.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 void Solution();
 
@@ -8,25 +9,34 @@ int main(int argc, char** argv) {
     Solution();
     return 0;
 }
-
-/// index starts from 1. tree[0] == tree.count
-void mid_order(int* tree, int index) {
-    if (index > tree[0]) return;
-    mid_order(tree, index * 2);
-    printf("%d ", tree[index]);
-    mid_order(tree, index * 2 + 1);
+// l=>vallidate, r=>vallidate
+int val_bst(int* arr, int n) {
+    int* stack = malloc(sizeof(int) * (n));
+    for(int i = 0; i < n; i++) stack[i] = INT_MIN;
+    int sp = 0;
+    int min_val = INT_MIN;
+    for(int i = 1; i <= n; i++) {
+        if (arr[i] < min_val) {
+            free(stack);
+            return -1;
+        }
+        while(stack[sp] < arr[i]) {
+            min_val = stack[sp--];
+        }
+        stack[++sp] = arr[i];
+    }
+    free(stack);
+    return 0;
 }
 
 void Solution() {
     int n;
     scanf("%d", &n);
-    int* tree = malloc(sizeof(int) * (n + 1));
-    tree[0] = n;
+    int* preorder_seq = malloc(sizeof(int) * (n + 1));
+    preorder_seq[0] = n;
     for (int i = 1; i <= n; i++) {
-        scanf("%d", &tree[i]);
+        scanf("%d", &preorder_seq[i]);
     }
-    mid_order(tree, 1);
-
-    int depth = ceil(log2(n));
-    printf("\n%d", depth);
+    printf(val_bst(preorder_seq, n) == 0 ? "YES" : "NO");
+    free(preorder_seq);
 }
